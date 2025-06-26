0a1ec08d465097b5d831df32dcfe24c73d8a8d71
YuliyaKomarova <julia.keehl1@gmail.com>
2025-06-26 13:01:06 +0000

    3.6.1

--- js/script.js
+++ js/script.js
@@ -89,3 +89,19 @@ console.log(headerMenu)
 
         }
 }
+// Preloader страницы
+const preloader = document.querySelector('.preloader');
+const content = document.querySelector('.content');
+if (preloader && content) {
+    setTimeout(() => {
+        // Скрываем прелоадер
+        preloader.style.opacity = '0';
+        preloader.style.visibility = 'hidden';
+
+        // Показываем контент
+        content.style.display = 'block';
+
+        // Удаляем элемент из DOM
+        preloader.remove();
+    }, 3000); // Задержка 3 секунды
+}
